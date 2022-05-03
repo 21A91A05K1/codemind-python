b16f97696addb873da462241757c80ab799abcfd
21A91A05K1 <97425986+21A91A05K1@users.noreply.github.com>
2022-05-03 13:52:50 +0530

    n=int(input()) c=0 ec=0 oc=0 while(n):     d=n%10     c+=1     n=n//10     if(d%2==0):         ec+=1     else:         oc+=1 if(ec==c):     print('Even') elif(oc==c):     print('Odd') else:     print('Mixed')

---
+++ Even_Odd_Mixed.py
@@ -0,0 +1,18 @@
+n=int(input())
+c=0
+ec=0
+oc=0
+while(n):
+    d=n%10
+    c+=1
+    n=n//10
+    if(d%2==0):
+        ec+=1
+    else:
+        oc+=1
+if(ec==c):
+    print('Even')
+elif(oc==c):
+    print('Odd')
+else:
+    print('Mixed')
